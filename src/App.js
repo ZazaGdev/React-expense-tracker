3e3184a332eb0b423613ff4b6003468df1982e63
ZazaGdev <Gejadze250@gmail.com>
2022-06-16 20:50:30 +0400

    Cleared up file system

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14),
+        },
+
+        {
+            id: 'e2',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12),
+        },
+    ];
+
     return (
         <div>
-            <h2>Let's get started!</h2>
-            <ExpenseItem></ExpenseItem>
+            <Expenses items={expenses} />
         </div>
     );
-}
+};
 
 export default App;
